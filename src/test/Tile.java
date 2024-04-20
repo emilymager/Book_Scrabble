c87b297a707e599d1f3813a98447efc0638afaab
emilymager <emilymager1234@gmail.com>
2024-04-20 15:42:12 +0300

    adding the class Tile

--- src/test/Tile.java
+++ src/test/Tile.java
@@ -1,7 +1,34 @@
 package test;
 
 
-public class Tile {
+import java.util.Objects;
 
-	
+public class Tile {
+    public final int score;
+    public final char letter;
+
+
+    private Tile(int score, char letter) {
+        this.score = score;
+        this.letter = letter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return score == tile.score && letter == tile.letter;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, letter);
+    }
+
+    public static class Bag {
+
+    }
+
+
 }
